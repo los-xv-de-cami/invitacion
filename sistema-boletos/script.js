131e440cc3c7137365a4ee191279ef1b765726a3
los-xv-de-cami <gerardhr@gmail.com>
2025-11-27 21:00:20 -0600

    Update Apps Script URL in configuration

--- sistema-boletos/script.js
+++ sistema-boletos/script.js
@@ -1,6 +1,6 @@
 // Configuration
 const CONFIG = {
-  APPS_SCRIPT_URL: 'TU_URL_DEL_APPS_SCRIPT_AQUI', // Reemplazar con tu URL
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxYZE0PDbGd3QV3BHWEeUkmml2Pbyzfex88TwU4lEiPrlUKzF_a3at6quijAkTYul8d/exec', // Reemplazar con tu URL
   EVENT_NAME: 'Los XV Años de Camila'
 };
 
